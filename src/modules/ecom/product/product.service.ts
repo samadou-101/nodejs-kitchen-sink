57afebbe2e25d974211747b08b694d06895e96fe
samadou <mossabm036@gmail.com>
2026-05-03 17:31:22 +0100

    feat(ecom): initial product service

--- src/modules/ecom/product/product.service.ts
+++ src/modules/ecom/product/product.service.ts
@@ -1,3 +1,39 @@
+import {
+  deleteProduct as deleteProductRepo,
+  findProductById as findProductByIdRepo,
+  insertProduct,
+  updateProduct as updateProductRepo,
+} from "./product.repo";
+import type { ProductData } from "./product.types";
 
+export async function createProduct(data: ProductData) {
+  try {
+    return await insertProduct(data);
+  } catch (error) {
+    throw new Error("Failed to create product", { cause: error });
+  }
+}
 
+export async function updateProduct(data: ProductData) {
+  try {
+    return await updateProductRepo(data);
+  } catch (error) {
+    throw new Error("Failed to update product", { cause: error });
+  }
+}
 
+export async function removeProduct(id: number) {
+  try {
+    return await deleteProductRepo(id);
+  } catch (error) {
+    throw new Error("Failed to remove product", { cause: error });
+  }
+}
+
+export async function getProductById(id: number) {
+  try {
+    return await findProductByIdRepo(id);
+  } catch (error) {
+    throw new Error("Failed to fetch product", { cause: error });
+  }
+}
