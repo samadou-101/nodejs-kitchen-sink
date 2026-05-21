f35885c124889632202a9f25df4381ae8d32cab9
samadou <mossabm036@gmail.com>
2026-05-21 22:15:38 +0100

    chore(frontend): scaffold for next to-implementfeatures

--- frontend/react/src/main.tsx
+++ frontend/react/src/main.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import { Toaster } from "#components/ui/sonner";
 import "./global.css";
+import Overview from "./modules/overview/Overview";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Overview />
     <Toaster />
   </StrictMode>,
 );
---
+++ frontend/react/src/modules/overview/Overview.tsx
@@ -0,0 +1,5 @@
+const Overview = () => {
+  return <div>test</div>;
+};
+
+export default Overview;
